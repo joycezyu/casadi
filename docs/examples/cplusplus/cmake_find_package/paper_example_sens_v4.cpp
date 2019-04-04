d8598bb41fc5842b79cbe50a6b566200cf12e938
Joyce Yu <zhouy1@andrew.cmu.edu>
2019-04-04 16:04:30 -0400

    working version of 2 by 2 symmetric linear system in sensitivity

--- docs/examples/cplusplus/cmake_find_package/paper_example_sens_v4.cpp
+++ docs/examples/cplusplus/cmake_find_package/paper_example_sens_v4.cpp
@@ -27,6 +27,7 @@
 #include <fstream>
 #include <ctime>
 #include <casadi/casadi.hpp>
+//#include <Eigen/Dense>
 //#include <casadi/casadi/core/sx.hpp>
 
 using namespace casadi;
@@ -40,27 +41,47 @@ int main() {
    *    s.t.    6*x1 + 3&x2 + 2*x3 - pi = 0
    *            p2*x1 + x2 - x3 - 1 = 0
    *            x1, x2, x3 >= 0
+   *
+   *
+   *
+   *  Sensitivity calculates M * ds = - N, where
+   *      [ W    A   -I  ]            [grad_x L   ]            [dx       ]
+   *  M = [ A^T  0    0  ]   ,  N  =  [       c   ]   ,  ds =  [d_lambda ]
+   *      [ V    0    X  ]            [       0   ]            [dv       ]
+   *
+   *  Instead of solving the nonsymmetric linear system, equivalently we solve
+   *
+   *  M = [ W + X^(-1)V     A  ]  ,   N =  [grad_x L   ] ,  ds = [dx       ]
+   *      [  A^T            0  ]           [       c   ]         [d_lambda ]
+   *
+   *  and dv = -X^(-1)V dx
+   *
+   *
   */
 
 
 
-  SX x1 = SX::sym("x1");
-  SX x2 = SX::sym("x2");
-  SX x3 = SX::sym("x3");
-  SX p  = SX::sym("p", 2);
+
+  /// Model construction
+
+
+  MX x1 = MX::sym("x1");
+  MX x2 = MX::sym("x2");
+  MX x3 = MX::sym("x3");
+  MX p  = MX::sym("p", 2);
 
 
   // Objective
-  SX f = x1*x1 + x2*x2 + x3*x3;
+  MX f = x1*x1 + x2*x2 + x3*x3;
 
   // Constraints
-  SX g = vertcat(
-         6*x1 + 3*x2 + 2*x3 - p(0),
+  MX g = vertcat(
+      6*x1 + 3*x2 + 2*x3 - p(0),
       p(1)*x1 +   x2 -   x3 -   1
   );
 
-  SX x = SX::vertcat({x1,x2,x3});
-  SXDict nlp = {{"x", x},
+  MX x = MX::vertcat({x1,x2,x3});
+  MXDict nlp = {{"x", x},
                 {"p", p},
                 {"f", f},
                 {"g", g}};
@@ -102,36 +123,47 @@ int main() {
   cout << setw(30) << "Primal solution: " << res.at("x") << endl;
   cout << setw(30) << "Dual solution (x): " << res.at("lam_x") << endl;
   cout << setw(30) << "Dual solution (g): " << res.at("lam_g") << endl;
+  cout << setw(30) << "Dual solution (p): " << res.at("lam_p") << endl;
 
   vector<DM> prim_param{res.at("x"), p1};
 
-  int ng = g.size1();  // ng = number of constraints g
-  SX lambda = SX::sym("lambda", ng);
-  SX lagrangian = f + dot(lambda, g);
 
-  SX grad = jacobian(g, x);
+  /// sensitivity calculation
+
+
+  int ng = g.size1();  // ng = number of constraints g
+  int nx = x.size1();
+  cout << "nx = " << nx << endl;
+  MX lambda = MX::sym("lambda", ng);
+  MX v      = MX::sym("v", nx);
+  MX V      = MX::diag(v);
+  MX Xinv   = MX::diag(1/x);
+  MX XiV    = mtimes(Xinv, V);
+  cout << " XiV = " << XiV << endl;
+  MX lagrangian = f + dot(lambda, g) - dot(v, x);
+
+  MX grad = jacobian(g, x);
   std::cout << "jacobians of constraints" << grad << std::endl;
 
   auto jac_lagrangian = jacobian(lagrangian, x);
   std::cout << "jacobians of lagrangian" << jac_lagrangian << std::endl;
 
-  // both the following two lines work to get the hessian
-  SX hess = jacobian(jac_lagrangian, x);
-  SX hess1 = hessian(lagrangian, x);
-  std::cout << hess1 << std::endl;
 
-  // Function, input, output, input-name, output-name
+  MX hess = hessian(lagrangian, x);
+  std::cout << hess << std::endl;
+
+  // Function_name(input, output, input-name, output-name)
   Function f_jac("grad",{x, p}, {grad}, {"x", "p"}, {"j"});
   auto j = f_jac(prim_param);
   cout << j << endl;
 
 
 
-  Function f_hess("hessian",{x, lambda}, {hess1});
+  Function f_hess("hessian",{x, lambda, v}, {hess});
   // using the following format to evaluate hessian if the input contains x and lambda
   // apparently either SX or DM datatype can work with res.at()
   //vector<SX> prim_dual{res.at("x"), res.at("lam_g")};
-  vector<DM> prim_dual{res.at("x"), res.at("lam_g")};
+  vector<DM> prim_dual{res.at("x"), res.at("lam_g"), res.at("lam_x")};
   auto h = f_hess(prim_dual);
   cout << h << endl;
 
@@ -140,33 +172,69 @@ int main() {
 
   /// Assemble KKT matrix
   // TODO: think about using sparsity zeros??
-  SX M0 = SX::zeros(ng, ng); // zero matrix at the bottom right
+  //DM M0 = DM::zeros(ng, ng);
+  // sparse zero matrix
+  MX M0 = MX(ng, ng);
+  //MX V = MX::diag(res.at("lam_x"));
+  //MX X_inv = MX::diag(1/res.at("x"));
+  //MX XiV = mtimes(X_inv, V);
+  //cout << "size XiV = " << XiV.size() << endl;
+  //cout << "X =   " << X << endl;
+  //cout << "inv(X) =   " << inv(X) << endl;
 
-  /// LHS
-  SX KKTprimer = SX::horzcat({SX::vertcat({hess1, grad}), SX::vertcat({grad.T(), M0})});
-  cout << KKTprimer << endl;
+  // LHS
+  MX KKTprimer = MX::horzcat({MX::vertcat({hess + XiV, grad}),
+                              MX::vertcat({grad.T(), M0})});
+
+  cout << "KKT = " << KKTprimer << endl;
   //Function KKT("KKT",{x, lambda}, {KKTprimer});
   //cout << KKT(prim_dual) << endl;
 
   /// RHS
-  SX phi = SX::vertcat({jac_lagrangian.T(), g});
-  SX sensitivity = solve(KKTprimer, -phi);
-  Function sens("sens",{x, lambda, p}, {sensitivity});
-  vector<DM> prim_dual_param{res.at("x"), res.at("lam_g"), p1};
+  MX phi = MX::vertcat({jac_lagrangian.T(), g});
+  MX sensitivity = solve(KKTprimer, -phi);
+  //MX sensitivity = solve(KKTprimer, -phi, "cssparse");
+  //MX sensitivity = solve(KKTprimer, -phi, "ldl");
+  //MX sensitivity = solve(KKTprimer, -phi, "qr");
+  Function sens_eval("sens", {x, lambda, v, p}, {sensitivity});
+  cout << "-1" << endl;
+  vector<DM> prim_dual_param{res.at("x"), res.at("lam_g"), res.at("lam_x"), p1};
 
-  Matrix<double> ds = DM::vertcat({sens(prim_dual_param)});
+  cout << "0" << endl;
+  DM dx_dl = DM::vertcat({sens_eval(prim_dual_param)});
+
+  cout << "1" << endl;
+  // construct dx (only for primal)
+  //cout << "dx = "  << ds(Slice(0, nx)) << endl;
+  MX dx = MX::sym("dx", nx);
+  MX dv = -mtimes(XiV, dx);
+  Function dv_eval("dv", {x, v, dx}, {dv});
+  vector<DM> x_v_dx{res.at("x"), res.at("lam_x"), dx_dl(Slice(0, nx))};
+  //MX dv = -mtimes(XiV, sensitivity(Slice(0, nx)));   // dv is of datatype MX, need to
+  DM dvv = DM::vertcat({dv_eval(x_v_dx)});
+  cout << "dv = " << dv << endl;
+  cout << "dv_eval = " << dvv << endl;
+  //Function dv_eval("dv", {x, lambda, v, p}, {dv});
+
+  //Eigen::MatrixXd dv(1, nx) = XiV * ds(Slice(0, nx));
+
+  DM ds = DM::vertcat({dx_dl, dvv});
+  cout << "2" << endl;
   cout << "ds = " << ds << endl;
   cout << ds.size() << endl;
 
-  Matrix<double> s = DM::vertcat({res.at("x"), res.at("lam_g")});
+  //Matrix<double> total_ds = MX::vertcat({ds, dv});
+  //cout << "dv = " << dv << endl;
+
+  DM s = DM::vertcat({res.at("x"), res.at("lam_g"), res.at("lam_x")});
   cout << s.size() << endl;
-  cout << s+ds << endl;
-  //cout << s + ds << endl;
+
+  Matrix<double> s1 = s+ds;
+  cout << s1 << endl;
 
 
 
 
 
-
-    return 0;
+  return 0;
 }
